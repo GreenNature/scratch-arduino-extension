b04f2dff07fc566abb14493cbc2d0d5682061cc3
GreenNature <nobu1015@naver.com>
2016-11-23 17:35:08 +0900

    Update arduino_extension.js

--- arduino_extension.js
+++ arduino_extension.js
@@ -485,12 +485,7 @@
     else
       return { status:2, msg:'Connected' };
   };
-  
-  ext.runMotion = function(motionNumber) {
-    var output = motionNumber;
-      return output;
-  };
-  
+
   ext._deviceRemoved = function(dev) {
     console.log('Device removed');
     // Not currently implemented with serial devices
@@ -575,8 +570,7 @@
       ['h', 'when analog %n %m.ops %n%', 'whenAnalogRead', 1, '>', 50],
       ['r', 'read analog %n', 'analogRead', 0],
       ['-'],
-      ['r', 'map %n from %n %n to %n %n', 'mapValues', 50, 0, 100, -240, 240],
-      ['r', 'run motion %n', 'runMotion', 0]
+      ['r', 'map %n from %n %n to %n %n', 'mapValues', 50, 0, 100, -240, 240]
     ],
     de: [
       ['h', 'Wenn Arduino verbunden ist', 'whenConnected'],
@@ -721,8 +715,7 @@
       ['h', '아날로그 %n 번 핀의 값이 %m.ops %n% 일 때', 'whenAnalogRead', 1, '>', 50],
       ['r', '아날로그 %n 번 핀의 값', 'analogRead', 0],
       ['-'],
-      ['r', '%n 을(를) %n ~ %n 에서 %n ~ %n 의 범위로 바꾸기', 'mapValues', 50, 0, 100, -240, 240],
-      ['r', 'run motion %n', 'runMotion', 0]
+      ['r', '%n 을(를) %n ~ %n 에서 %n ~ %n 의 범위로 바꾸기', 'mapValues', 50, 0, 100, -240, 240]
     ],
     nb: [
       ['h', 'når enheten tilkobles', 'whenConnected'],
@@ -1105,7 +1098,7 @@
     blocks: blocks[lang],
     menus: menus[lang],
     url: 'http://khanning.github.io/scratch-arduino-extension'
-  }
+  };
 
   ScratchExtensions.register('Arduino', descriptor, ext, {type:'serial'});
 
